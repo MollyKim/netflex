369beb6a2cca36ec36dcece1839fa749bb486db1
MollyKim <kimmh3876@gmail.com>
2020-08-10 17:29:01 +0900

    Start to make netflex demo

---
+++ netflex/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:netflex/widget/bottombar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Netflex',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              Container(),
+              Container(),
+              Container(),
+              Container(),
+            ],
+          ),
+          bottomNavigationBar: BottomBar(),
+        ),
+      ),
+    );
+  }
+}
